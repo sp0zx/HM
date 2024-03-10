53223a75f0800a052867c7780d697810eb009814
sp0zx <122081930+sp0zx@users.noreply.github.com>
2024-03-10 17:22:56 -0400

    hwid.lua

---
+++ hwid.lua
@@ -0,0 +1,13 @@
+local ejfgbdjs = game.Players
+local SDIKjFNIEJSMNFdiks = {
+"",
+"",
+"",
+"",
+""
+}
+if SDIKjFNIEJSMNFdiks[table.find(SDIKjFNIEJSMNFdiks,game:GetService("RbxAnalyticsService"):GetClientId())] == game:GetService("RbxAnalyticsService"):GetClientId() then
+    script()
+    else
+    ejfgbdjs.LocalPlayer:Kick("Error: 163")
+end
