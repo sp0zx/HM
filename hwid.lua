1aeee5c68fbff2fc017867768ea5fcf812981721
sp0zx <122081930+sp0zx@users.noreply.github.com>
2024-03-12 15:03:45 -0400

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,7 +1,7 @@
 local ejfgbdjs = game.Players
 local SDIKjFNIEJSMNFdiks = {
 "cdd31d8b-2cd5-4634-a609-67a98894fac3",
-"",
+"4BD3953E-2935-49C5-AAC2-8A41E701AC07",
 ""
 }
 if SDIKjFNIEJSMNFdiks[table.find(SDIKjFNIEJSMNFdiks,game:GetService("RbxAnalyticsService"):GetClientId())] == game:GetService("RbxAnalyticsService"):GetClientId() then
