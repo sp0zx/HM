b7882e75e594e31ef6d6812692cd384c5401c822
sp0zx <122081930+sp0zx@users.noreply.github.com>
2024-03-16 03:51:31 -0400

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -2,7 +2,8 @@ local ejfgbdjs = game.Players
 local SDIKjFNIEJSMNFdiks = {
 "cdd31d8b-2cd5-4634-a609-67a98894fac3", -- jojos wl
 "740A5197-A99B-4829-93ED-E2F6D1F79E76", -- edis wl
-"9B62A311-E417-4339-BE18-045EA207B185" -- @'s wl
+"9B62A311-E417-4339-BE18-045EA207B185", -- @'s wl
+"EDE5AF96-7E50-43BF-AE6E-7AD3B0EA8CA1" -- tester | scott
 }
 if SDIKjFNIEJSMNFdiks[table.find(SDIKjFNIEJSMNFdiks,game:GetService("RbxAnalyticsService"):GetClientId())] == game:GetService("RbxAnalyticsService"):GetClientId() then
     script()
