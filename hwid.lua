e6aadad234d6c2f2218bffd2fc36af3424b7a484
sp0zx <122081930+sp0zx@users.noreply.github.com>
2024-03-17 22:23:35 -0400

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,7 +1,8 @@
 local ejfgbdjs = game.Players
 local SDIKjFNIEJSMNFdiks = {
 "cdd31d8b-2cd5-4634-a609-67a98894fac3", -- jojos wl
---"F8A30C51-65D2-4ABA-B97C-FC965F6370FF", -- edis wl
+"1979AE6A-3E69-48D7-9BC5-CB24BC7D2801", -- jojos wl again but for like ro exec
+"F8A30C51-65D2-4ABA-B97C-FC965F6370FF", -- edis wl
 "9B62A311-E417-4339-BE18-045EA207B185", -- @'s wl
 "EDE5AF96-7E50-43BF-AE6E-7AD3B0EA8CA1" -- tester | scott
 }
