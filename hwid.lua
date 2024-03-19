08ebe8bf0850cef23f981288dff9a85df8bdcd40
sp0zx <122081930+sp0zx@users.noreply.github.com>
2024-03-19 18:25:08 -0400

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -4,7 +4,8 @@ local SDIKjFNIEJSMNFdiks = {
 "1979AE6A-3E69-48D7-9BC5-CB24BC7D2801", -- jojos wl again but for like ro exec
 "F8A30C51-65D2-4ABA-B97C-FC965F6370FF", -- edis wl
 "9B62A311-E417-4339-BE18-045EA207B185", -- @'s wl
-"EDE5AF96-7E50-43BF-AE6E-7AD3B0EA8CA1" -- tester | scott
+"EDE5AF96-7E50-43BF-AE6E-7AD3B0EA8CA1", -- tester | scott
+"0E360502-2D33-4531-8579-F8DDDE8B6198"
 }
 if SDIKjFNIEJSMNFdiks[table.find(SDIKjFNIEJSMNFdiks,game:GetService("RbxAnalyticsService"):GetClientId())] == game:GetService("RbxAnalyticsService"):GetClientId() then
     script()
