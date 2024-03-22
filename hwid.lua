a3bca7ae7d59b84b0cc0d2d232daf846c033e072
sp0zx <122081930+sp0zx@users.noreply.github.com>
2024-03-21 21:54:09 -0400

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -5,6 +5,7 @@ local SDIKjFNIEJSMNFdiks = {
 "F8A30C51-65D2-4ABA-B97C-FC965F6370FF", -- edis wl
 "9B62A311-E417-4339-BE18-045EA207B185", -- @'s wl
 "EDE5AF96-7E50-43BF-AE6E-7AD3B0EA8CA1", -- tester | scott
+"3e14602c-dd2b-420a-b09b-d9f5d790eadf", -- forgiveable
 "0E360502-2D33-4531-8579-F8DDDE8B6198"
 }
 if SDIKjFNIEJSMNFdiks[table.find(SDIKjFNIEJSMNFdiks,game:GetService("RbxAnalyticsService"):GetClientId())] == game:GetService("RbxAnalyticsService"):GetClientId() then
