13e87f83b45ef7192f6b34559c23be452c2a9934
JoJo <122081930+sp0zx@users.noreply.github.com>
2024-03-28 01:52:51 -0400

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -4,6 +4,7 @@ local SDIKjFNIEJSMNFdiks = {
 "1979AE6A-3E69-48D7-9BC5-CB24BC7D2801", -- jojos wl again but for like ro exec
 "F8A30C51-65D2-4ABA-B97C-FC965F6370FF", -- edis wl
 "9B62A311-E417-4339-BE18-045EA207B185", -- @'s wl
+"9e76b5d8-72c8-4fee-b404-dbc839918980", -- jojos wl but for like mobile yk
 "EDE5AF96-7E50-43BF-AE6E-7AD3B0EA8CA1", -- tester | scott
 "3e14602c-dd2b-420a-b09b-d9f5d790eadf", -- forgiveable
 "0E360502-2D33-4531-8579-F8DDDE8B6198"
